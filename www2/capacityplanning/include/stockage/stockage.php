3c5e807a2d816234e20c83959f476043f2d31865
Batouba <baptiste.barbarin@yahoo.fr>
2017-02-13 21:59:43 +0100

    Implementation de la page stockage

--- www2/capacityplanning/include/stockage/stockage.php
+++ www2/capacityplanning/include/stockage/stockage.php
@@ -53,17 +53,14 @@
 	}
 
 	.tableau_meteo {
-		width: 25%;
-		margin-left: 37.5%;
+		width: 50%;
+		margin-left: 25%;
 		background-color: #66A3C7;
 	}
 
 	.tableau_meteo td {
 	  	font-size: 16px;
 	 	font-weight: bold;
-	}
-
-	.tableau_meteo td {
 		text-align: center;
 		color: white;
 	}
@@ -73,28 +70,112 @@
 	<?=date("d/m/Y")?>
 </div>
 
+<script type="text/javascript">
+	function redirection()
+	{
+		window.location.href = window.location.href + '&type=' + document.getElementById("menu_deroulant").value;
+	}
+</script>
+
+<?php
+	$type = "SI";
+
+	if (isset($_GET['type'])) {
+		$type = $_GET['type'];
+	}
+
+	if ($type == "SI") {
+		$select_SI = "selected";
+	}
+
+	if ($type == "data_center") {
+		$select_data_center = "selected";
+	}
+
+	if ($type == "baie_ipstor") {
+		$select_baie_ipstor = "selected";
+	}
+?>
+
 <center>
-	<select name="menu_deroulant">
-		<option>SI
-		<option>Data Center
-		<option>Baie/IPSTOR
+	<select name="menu_deroulant" id="menu_deroulant" onChange="redirection();">
+		<option value="SI" <?=$select_SI?>>SI</option>
+		<option value="data_center" <?=$select_data_center?>>Data Center</option>
+		<option value="baie_ipstor" <?=$select_baie_ipstor?>>Baie/IPSTOR</option>
 	</select>
 </center></br></br>
 
-<table class="tableau_meteo">
-  	<tr>
-		<th></th><th></th>
-	</tr>
-  	<tr>
-  		<td>Virtualisation</td><td><img src="images/nuageux.png"></td>
-  	</tr>
-  	<tr>
-		<td>Stockage</td><td><img src="images/pluvieux.png"></td>
-	</tr>
-  	<tr>
-		<td>Sauvegarde des VMs</td><td><img src="images/soleil.png"></td>
-	</tr>
-  	<tr>
-		<td>Sauvegarde</td><td><img src="images/nuageux.png"></td>
-	</tr>
-</table>
+<?php
+	if ($type == "SI") {
+?>
+	<table class="tableau_meteo">
+	  	<tr>
+			<th></th><th></th><th></th><th></th>
+		</tr>
+	  	<tr>
+	  		<td style="color: black;" colspan=2>SNP1</td><td style="color: black;" colspan=2>SNP2</td>
+	  	</tr>
+	  	<tr>
+	  		<td>Virtualisation</td><td><img src="images/nuageux.png"></td><td>Virtualisation</td><td><img src="images/soleil.png"></td>
+	  	</tr>
+	  	<tr>
+			<td>Stockage</td><td><img src="images/pluvieux.png"></td><td>Stockage</td><td><img src="images/nuageux.png"></td>
+		</tr>
+	  	<tr>
+			<td>Sauvegarde des VMs</td><td><img src="images/soleil.png"></td><td>Sauvegarde des VMs</td><td><img src="images/soleil.png"></td>
+		</tr>
+	  	<tr>
+			<td>Sauvegarde</td><td><img src="images/nuageux.png"></td><td>Sauvegarde</td><td><img src="images/pluvieux.png"></td>
+		</tr>
+	</table>
+<?php
+	}
+
+	if ($type == "data_center") {
+?>
+	<table class="tableau_meteo">
+	  	<tr>
+			<th></th><th></th><th></th><th></th>
+		</tr>
+	  	<tr>
+	  		<td style="color: black;" colspan=2>SNP1 - Clusters</td><td style="color: black;" colspan=2>SNP2 - Clusters</td>
+	  	</tr>
+	  	<tr>
+	  		<td>NUTAMPPRD</td><td><img src="images/soleil.png"></td><td>NUTAMPPRD</td><td><img src="images/soleil.png"></td>
+	  	</tr>
+	  	<tr>
+			<td>ORAAMPPRD</td><td><img src="images/pluvieux.png"></td><td>ORAAMPPRD</td><td><img src="images/nuageux.png"></td>
+	  	<tr>
+			<td>VSPAMPRD</td><td><img src="images/nuageux.png"></td><td>VSPAMPRD</td><td><img src="images/pluvieux.png"></td>
+		</tr>
+	  	<tr>
+			<td>VSPPRDSII</td><td><img src="images/pluvieux.png"></td><td>VSPPRDSII</td><td><img src="images/pluvieux.png"></td>
+		</tr>
+	</table>
+<?php
+	}
+
+	if ($type == "baie_ipstor") {
+?>
+	<table class="tableau_meteo">
+	  	<tr>
+			<th></th><th></th><th></th><th></th>
+		</tr>
+	  	<tr>
+	  		<td style="color: black;" colspan=2>SNP1 - Baies</td><td style="color: black;" colspan=2>SNP2 - Baies</td>
+	  	</tr>
+	  	<tr>
+	  		<td>AXIOM600BENAMP</td><td><img src="images/soleil.png"></td><td>AXIOM600BENAMP</td><td><img src="images/pluvieux.png"></td>
+	  	</tr>
+	  	<tr>
+			<td>FSONE316AMP</td><td><img src="images/pluvieux.png"></td><td>FSONE316AMP</td><td><img src="images/nuageux.png"></td>
+		</tr>
+	  		<td style="color: black;" colspan=2>SNP1 - IPSTOR</td><td style="color: black;" colspan=2>SNP2 - IPSTOR</td>
+	  	</tr>
+	  	<tr>
+			<td>FALCONV8_AMP</td><td><img src="images/nuageux.png"></td><td>FALCONV8_AMP</td><td><img src="images/pluvieux.png"></td>
+		</tr>
+	</table>
+<?php
+	}
+?>
