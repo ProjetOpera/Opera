5653c9fa3f6697ed4e48df725d733235bb7dac95
AdrienTODA <adrientoda@gmail.com>
2017-03-13 23:48:10 +0100

    Rajouts structure

---
+++ www2/capacityplanning/include/stockage/parametres.php
@@ -0,0 +1,17 @@
+<table class='display_list2' cellpadding=0 cellspacing=0 border=0>
+	<tr class='table_line'>
+		<td>Module</td>
+		<td>Equipement</td>
+		<td>Seuil</td>
+		<td>Alerte</td>
+		<td style="text-align: center;" colspan=2>Actions</td>
+	</tr>
+	<tr class='line1'>
+		<td>Stockage</td>
+		<td>AXIOM600</td>
+		<td>4 To</td>
+		<td>3 To</td>
+		<td style="text-align: center;"><img src='../commun/images/save.png'/></td>
+		<td style="text-align: center;"><a class='various1 delete' href=''></a></td>
+	</tr>
+</table>
--- www2/capacityplanning/include/stockage/stockage.php
+++ www2/capacityplanning/include/stockage/stockage.php
@@ -3,8 +3,8 @@
 	// Affichage tableau app //
 	///////////////////////////
 	
-	/*
-	$sql_recup_app="SELECT name, role, localisation FROM inv_san.baie WHERE date_releve = (SELECT MAX(date_releve) FROM inv_san.baie) ORDER BY name;";
+	
+	/*$sql_recup_app="SELECT name, role, localisation FROM inv_san.baie WHERE date_releve = (SELECT MAX(date_releve) FROM inv_san.baie) ORDER BY name;";
 
 	$result_recup_app = $ressourceBDD_appli->query($sql_recup_app);
 	
@@ -42,8 +42,8 @@
 		echo $contenu_tab_app;	
 		
 		echo "</table>\n";
-	}
-	*/
+	}*/
+	
 ?>
 <style type="text/css">
 	.date_jour {
@@ -73,15 +73,28 @@
 <script type="text/javascript">
 	function redirection()
 	{
-		window.location.href = window.location.href + '&type=' + document.getElementById("menu_deroulant").value;
+		window.location.href = window.location.href + '&type=' + document.getElementById("menu_deroulant").value + '&target=' + document.getElementById("menu_deroulant").value;
 	}
 </script>
 
 <?php
 	$type = "SI";
+	//$target = "SNP1";
 
 	if (isset($_GET['type'])) {
 		$type = $_GET['type'];
+	}
+	
+	if (isset($_GET['target'])) {
+		$target = $_GET['target'];
+	}
+	
+	if ($target == "SNP1") {
+		$select_SNP1 = "selected";
+	}
+
+	if ($target == "SNP2") {
+		$select_SNP2 = "selected";
 	}
 
 	if ($type == "SI") {
@@ -119,7 +132,7 @@
 	  		<td>Virtualisation</td><td><img src="images/nuageux.png"></td><td>Virtualisation</td><td><img src="images/soleil.png"></td>
 	  	</tr>
 	  	<tr>
-			<td>Stockage</td><td><img src="images/pluvieux.png"></td><td>Stockage</td><td><img src="images/nuageux.png"></td>
+			<td><a href="http://localhost/capacityplanning/id_menu=228&type=data_center&target=SNP1" target="_self">Stockage</a></td><td><img src="images/pluvieux.png"></td><td><a href="http://localhost/capacityplanning/id_menu=228&type=data_center&target=SNP2" target="_self">Stockage</a></td><td><img src="images/nuageux.png"></td>
 		</tr>
 	  	<tr>
 			<td>Sauvegarde des VMs</td><td><img src="images/soleil.png"></td><td>Sauvegarde des VMs</td><td><img src="images/soleil.png"></td>
@@ -127,11 +140,12 @@
 	  	<tr>
 			<td>Sauvegarde</td><td><img src="images/nuageux.png"></td><td>Sauvegarde</td><td><img src="images/pluvieux.png"></td>
 		</tr>
-	</table>
+	</table>	
+	
 <?php
 	}
 
-	if ($type == "data_center") {
+	if ($type == "data_center" && $target == "data_center") {
 ?>
 	<table class="tableau_meteo">
 	  	<tr>
@@ -154,7 +168,61 @@
 	</table>
 <?php
 	}
+?>
 
+<?php
+	if ($type == "data_center" && $target == "SNP1") {
+?>
+	<table class="tableau_meteo">
+	  	<tr>
+			<th></th><th></th>
+		</tr>
+	  	<tr>
+	  		<td style="color: black;" colspan=2>SNP1 - Clusters</td>
+	  	</tr>
+	  	<tr>
+	  		<td>NUTAMPPRD</td><td><img src="images/soleil.png"></td>
+	  	</tr>
+	  	<tr>
+			<td>ORAAMPPRD</td><td><img src="images/pluvieux.png"></td>
+	  	<tr>
+			<td>VSPAMPRD</td><td><img src="images/nuageux.png"></td>
+		</tr>
+	  	<tr>
+			<td>VSPPRDSII</td><td><img src="images/pluvieux.png"></td>
+		</tr>
+	</table>
+<?php
+	}
+?>
+
+<?php
+	if ($type == "data_center" && $target == "SNP2") {
+?>
+	<table class="tableau_meteo">
+	  	<tr>
+			<th></th><th></th>
+		</tr>
+	  	<tr>
+	  		<td style="color: black;" colspan=2>SNP2 - Clusters</td>
+	  	</tr>
+	  	<tr>
+	  		<td>NUTAMPPRD</td><td><img src="images/soleil.png"></td>
+	  	</tr>
+	  	<tr>
+			<td>ORAAMPPRD</td><td><img src="images/nuageux.png"></td>
+	  	<tr>
+			<td>VSPAMPRD</td><td><img src="images/pluvieux.png"></td>
+		</tr>
+	  	<tr>
+			<td>VSPPRDSII</td><td><img src="images/pluvieux.png"></td>
+		</tr>
+	</table>
+<?php
+	}
+?>
+
+<?php
 	if ($type == "baie_ipstor") {
 ?>
 	<table class="tableau_meteo">
