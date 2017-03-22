f49275fbabc2bd28a47d6f2ed24fc03c09992173
Batouba <baptiste.barbarin@yahoo.fr>
2017-03-22 17:35:12 +0100

    [Java] Modif dans le calcul

--- java/src/tests/Test.java
+++ java/src/tests/Test.java
@@ -1,5 +1,6 @@
 package tests;
 
+import java.util.Date;
 import java.util.HashMap;
 
 import util.PointXY;
@@ -11,13 +12,13 @@ public class Test {
 		
 		//Instantiation du hashmap pour test
 		//liste_PointXY.put(id_SQL, new PointXY(datetimestamp, valeur));
-		liste_PointXY.put(1, new PointXY(1489532400, 200));
-		liste_PointXY.put(2, new PointXY(1489618800, 180));
-		liste_PointXY.put(3, new PointXY(1489705200, 185));
-		liste_PointXY.put(4, new PointXY(1489791600, 175));
-		liste_PointXY.put(5, new PointXY(1489878000, 172));
+		liste_PointXY.put(1, new PointXY(1489532400, 160));
+		liste_PointXY.put(2, new PointXY(1489618800, 150));
+		liste_PointXY.put(3, new PointXY(1489705200, 149));
+		liste_PointXY.put(4, new PointXY(1489791600, 148));
+		liste_PointXY.put(5, new PointXY(1489878000, 147));
 		
-		for(int i = 1; i <= 30; i++)
+		for(int i = 1; i <= 90; i++)
 		{
 			PointXY PointXYSuivant = CalculPrevisionnel.PointXY_suivant(liste_PointXY);
 			liste_PointXY.put(liste_PointXY.size()+1, PointXYSuivant);
--- java/src/util/PointXY.java
+++ java/src/util/PointXY.java
@@ -1,10 +1,10 @@
 package util;
 
 public class PointXY {
-	public float X;
+	public int X;
 	public float Y;
 	
-	public PointXY(float new_X, float new_Y) {
+	public PointXY(int new_X, float new_Y) {
 		X = new_X;
 		Y = new_Y;
 	}
