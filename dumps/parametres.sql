c23b218a5ab0c5afb264ef0b6394bf0627e822da
AdrienTODA <adrientoda@gmail.com>
2017-04-07 14:04:19 +0200

    [SQL]Reupload table parametres

--- dumps/parametres.sql
+++ dumps/parametres.sql
@@ -3,6 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
+-- Généré le :  Ven 07 Avril 2017 à 12:02
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -15,7 +16,16 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-DROP TABLE IF EXISTS `parametres`;
+--
+-- Base de données :  `capacityplanning`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `parametres`
+--
+
 CREATE TABLE `parametres` (
   `id` int(11) NOT NULL,
   `Module_concerne` varchar(50) NOT NULL,
@@ -30,12 +40,32 @@ CREATE TABLE `parametres` (
   `Custom5` varchar(100) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `parametres`
+--
+
+INSERT INTO `parametres` (`id`, `Module_concerne`, `Seuil`, `Alerte`, `Label`, `Pourcentage`, `Custom1`, `Custom2`, `Custom3`, `Custom4`, `Custom5`) VALUES
+(2, 'TSM', 40, 30, 'BD', 0, NULL, NULL, NULL, NULL, NULL);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `parametres`
+--
 ALTER TABLE `parametres`
   ADD PRIMARY KEY (`id`);
-  
-ALTER TABLE `parametres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `parametres`
+--
+ALTER TABLE `parametres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
