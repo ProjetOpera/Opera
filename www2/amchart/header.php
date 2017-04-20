ca1836e8395e552350252b162aba127abf8cf226
Batouba <baptiste.barbarin@yahoo.fr>
2017-04-20 19:54:16 +0200

    [PHP] Reglage Bugs

--- www2/amchart/header.php
+++
@@ -1,9 +0,0 @@
-<html>
-	<head>
-		<title>Cahier Electronique CDE</title>
-
-  		<script src="amcharts/amcharts.js" type="text/javascript"></script>
-		<script src="amcharts/serial.js" type="text/javascript"></script> 
-		<script src="amcharts/lang/fr.js" type="text/javascript"></script> 
-	</head>
-</html>
