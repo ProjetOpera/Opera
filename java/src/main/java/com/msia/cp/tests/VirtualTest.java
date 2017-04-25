23ae6d8d391d67eaf4fe64e1179d935edf861cee
CendriNoir <faeh@live.fr>
2017-04-25 20:07:46 +0200

    [Java] Début implémentation log slf4j
    Conf log ok

--- java/src/main/java/com/msia/cp/tests/VirtualTest.java
+++ java/src/main/java/com/msia/cp/tests/VirtualTest.java
@@ -1,16 +1,22 @@
 package com.msia.cp.tests;
 
 import com.msia.cp.dao.VirtualDaoImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by A618735 on 07/04/2017.
  */
 public class VirtualTest {
+    private static Logger logger = LoggerFactory.getLogger(VirtualTest.class);
     public static void main(String[] args) {
             VirtualDaoImpl virtual = new VirtualDaoImpl();
             System.out.println("Taille de la liste de virtualisation : " +virtual.findAllVirtual().size());
 
-            for (Object o : virtual.findAllVirtual())
-                System.out.println("  " + o.toString());
+            /*for (Object o : virtual.findAllVirtual())
+                System.out.println("  " + o.toString());*/
+
+
+            logger.info("test");
     }
 }
--- java/src/main/java/com/msia/cp/tests/VueGlobaleTest.java
+++ java/src/main/java/com/msia/cp/tests/VueGlobaleTest.java
@@ -1,22 +1,23 @@
 package com.msia.cp.tests;
 
 import com.msia.cp.dao.VueGlobaleDaoImpl;
-import com.msia.cp.entities.VueGlobaleEntity;
-import com.msia.cp.util.EnregistrementBdd;
-
-import java.sql.Timestamp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Cendri on 30/03/2017.
  */
 public class VueGlobaleTest {
 
+    private static Logger logger = LoggerFactory.getLogger(VueGlobaleTest.class);
+
     public static void main(String[] args) {
+
+        logger.info("coucou");
         VueGlobaleDaoImpl vueG = new VueGlobaleDaoImpl();
         System.out.println("Taille de la liste vueG avant : " + vueG.findAllVueGlobale().size());
         //EnregistrementBdd.tsmVersVueGlobale();
         System.out.println("Taille de la liste vueG après : " + vueG.findAllVueGlobale().size());
-
 
 
         /*VueGlobaleEntity vue = new VueGlobaleEntity();
@@ -45,6 +46,7 @@ public class VueGlobaleTest {
             System.out.println("  " + o.toString());
         }
 
+        logger.info("pouet");
     }
 
 }
