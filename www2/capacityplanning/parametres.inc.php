86c9dda3b48673ecc1126f73af42d6f0c9a292d3
AdrienTODA <adrientoda@gmail.com>
2017-07-02 13:51:52 +0200

    [PHP]Ajout d'id textuels

--- www2/capacityplanning/parametres.inc.php
+++ www2/capacityplanning/parametres.inc.php
@@ -7,4 +7,7 @@ $id_textuel_menu_sauvegarde = 'cc_sauvegarde';
 $id_textuel_menu_veeam = 'cc_veeam';
 $id_textuel_menu_stockage = 'cc_stockage';
 $id_textuel_menu_virtu = 'cc_virtualisation';
+$id_textuel_menu_datacenter = 'cc_datacenter';
+$id_textuel_menu_DCAmpere = 'cc_dcampere';
+$id_textuel_menu_DCFranklin = 'cc_dcfranklin';
 ?>
